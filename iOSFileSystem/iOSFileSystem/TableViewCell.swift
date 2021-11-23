34447f7523168711eca000b9cc1c035e3b5cf678
Ebtesam Alahmari <ebtesam@Ebtesam-MacBook-Pro.local>
2021-11-23 23:30:48 +0300

    first commit

---
+++ iOSFileSystem/iOSFileSystem/TableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewCell.swift
+//  iOSFileSystem
+//
+//  Created by Ebtesam Alahmari on 23/11/2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lblFolderNames: UILabel!
+    
+}
