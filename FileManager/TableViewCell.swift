0351fca1d3ff10427799e2e5b769a683637e5839
Mola <aml@MacBook-Pro-alkhas-b-aml.local>
2021-11-24 01:38:25 +0300

    Amal Alghofaily

---
+++ FileManager/TableViewCell.swift
@@ -0,0 +1,15 @@
+
+import UIKit
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var celLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
